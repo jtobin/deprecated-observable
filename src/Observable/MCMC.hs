fd10079931eba2a8d613c84ebcf78043ae577507
Jared Tobin <jared@jtobin.ca>
2013-11-30 11:31:53 +1300

    Refine transition operator type, add operator composition.

--- src/Observable/MCMC.hs
+++ src/Observable/MCMC.hs
@@ -1,16 +1,9 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE ImpredicativeTypes #-}
-
--- The state components that you want to keep track of:
---   - parameter space position 
---   - range space position (and gradient?  do you really need to cache this?)
---   - additional state piece
 
 module Observable.MCMC where
 
-import Control.Arrow
 import Control.Lens
 import Control.Monad.Primitive
 import Control.Monad.State.Strict
@@ -18,7 +11,10 @@ import Observable.Core
 import Statistics.Distribution
 import Statistics.Distribution.Normal
 
--- | A Target consists of an objective function and possibly its gradient.
+-- testing
+import Pipes
+import System.Random.MWC hiding (uniform)
+
 data Target a = Target {
     _objective :: [a] -> Double
   , _gradient  :: Maybe ([a] -> [a])
@@ -33,6 +29,9 @@ data Trace a e = Trace {
 instance Show a => Show (Trace a e) where
   show = show . _parameterSpacePosition
 
+type TransitionOperator a e =
+  forall m. PrimMonad m => Target a -> StateT (Trace a e) (Observable m) [a]
+
 makeLenses ''Trace
 makeLenses ''Target
 
@@ -42,18 +41,33 @@ createTargetWithGradient f g  = Target f (Just g)
 createTargetWithoutGradient :: ([a] -> Double) -> Target a
 createTargetWithoutGradient f = Target f Nothing
 
-type TransitionOperator a m e = forall b. StateT (Trace a e) m b
+initializeTrace :: Target a -> [a] -> e -> Trace a e
+initializeTrace t as = Trace as (t^.objective $ as)
+
+-- | Sample from some distribution indirectly via MCMC.
+observeIndirectly :: Monad m => t -> (t -> StateT s m a) -> s -> Int -> m s
+observeIndirectly f t o n = replicateM_ n (t f) `execStateT` o
+
+-- | Better infix syntax for observeIndirectly.
+observedIndirectlyBy :: Monad m => t -> (t -> StateT s m a) -> s -> Int -> m s
+observedIndirectlyBy = observeIndirectly
+
+-- | Transition operator composition.
+interleave :: Monad m => (t -> m a) -> (t -> m b) -> t -> m b
+interleave s0 s1 t = s0 t >> s1 t
 
 
-
-
+-- testing
 
 isoGauss xs mu s = product $ zipWith density nds xs
   where nds = map (`normalDistr` s) mu
 
-perturb q e = mapM (\m -> normal m e) q
+perturb q e = mapM (`normal` e) q
 
-metropolisTransition t e = do
+metropolisTransition
+  :: Double
+  -> TransitionOperator Double Double
+metropolisTransition e t = do
   Trace q _ _ <- get
   zc          <- lift $ uniform (0, 1)
   proposal    <- lift $ perturb q e
@@ -61,7 +75,8 @@ metropolisTransition t e = do
   let next | zc < acceptProb = proposal
            | otherwise       = q
 
-      acceptProb = if isNaN arRatio then 0 else arRatio
+      acceptProb | isNaN arRatio = 0
+                 | otherwise     = arRatio
 
       arRatio = exp . min 0 $
           (t^.objective) proposal + log (isoGauss q proposal e)
@@ -70,13 +85,10 @@ metropolisTransition t e = do
   put $ Trace next (t^.objective $ next) e
   return next
 
-
-testTransition t = metropolisTransition t 0.1 >> metropolisTransition t 1.0
-
-
-
-
--- testing
+compositeMetropolis :: TransitionOperator Double Double
+compositeMetropolis = metropolisTransition 1.0 
+         `interleave` metropolisTransition 0.5 
+         `interleave` metropolisTransition 0.1 
 
 -- | log-Rosenbrock function.
 lRosenbrock :: RealFloat a => [a] -> a
@@ -92,5 +104,14 @@ glRosenbrock [x, y] =
 logRosenbrock :: Target Double
 logRosenbrock = createTargetWithGradient lRosenbrock glRosenbrock
 
-rTrace = Trace [0.0, 0.0] (lRosenbrock [0.0, 0.0]) 0.5
+-- you ideally want to get rid of the 'trace' at this point.. hmmm
+logRosenbrockVariate
+  :: PrimMonad m => Trace Double Double -> Int -> Observable m (Trace Double Double)
+logRosenbrockVariate = logRosenbrock `observedIndirectlyBy` compositeMetropolis
+
+q0 = Trace [0.0, 0.0] (lRosenbrock [0.0, 0.0]) 0.5
+
+main = do
+  g <- create
+  runEffect $ observe (logRosenbrockVariate q0 100) g >-> printer
 
